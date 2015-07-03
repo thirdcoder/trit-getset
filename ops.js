506d473610b9c66260ae2d95004cfc22021a1b29
Unknown Coder <@>
2015-07-03 12:47:23 -0700

    Add get_trit, clear_trit, set_trit, and trit_slice from 3502.js

--- ops.js
+++ ops.js
@@ -1,3 +1,41 @@
 'use strict';
 
+var bts2n = require('balanced-ternary').bts2n;
+var n2bts = require('balanced-ternary').n2bts;
+var N_TO_BT_DIGIT = require('balanced-ternary').N_TO_BT_DIGIT;
+var BT_DIGIT_TO_N = require('balanced-ternary').BT_DIGIT_TO_N;
 
+// get trit value at ith index of n, i of 0=least significant
+function get_trit(n,i) {
+  // convert entire number to balanced ternary string then slice
+  // would be nice to extract without converting everything, see extract_digit(), which
+  // works for unbalanced ternary, but balanced converts 2 -> 1i, so individual digit extraction
+  // is more difficult -- see https://en.wikipedia.org/wiki/Balanced_ternary#Conversion_from_ternary
+  var s = n2bts(n);
+  return ~~BT_DIGIT_TO_N[s.charAt(s.length - i - 1)];
+}
+
+// set ith trit (0=lst) of n to zero
+function clear_trit(n,i) {
+  var t0 = get_trit(n,i);
+  var t_value = Math.pow(3,i) * t0;
+  return n - t_value;
+}
+
+// set ith trit (0=lst) of n to trit t=-1, 0, or 1
+function set_trit(n,i,t) {
+  return clear_trit(n,i) + Math.pow(3,i) * t;
+}
+
+// extract trits of n from range a to b (0=lst)
+function trit_slice(n,a,b) {
+  var s = n2bts(n);
+  var ss = s.substring(s.length - b, s.length - a);
+  return bts2n(ss);
+}
+module.exports = {
+  get_trit: get_trit,
+  clear_trit: clear_trit,
+  set_trit: set_trit,
+  trit_slice: trit_slice
+};
--- test.js
+++ test.js
@@ -1,5 +1,46 @@
 'use strict';
 
 var test = require('tape');
+
+var n2bts = require('balanced-ternary').n2bts;
+var bts2n = require('balanced-ternary').bts2n;
+
 var ops = require('./');
+
+var get_trit = ops.get_trit;
+var clear_trit = ops.clear_trit;
+var set_trit = ops.set_trit;
+var trit_slice = ops.trit_slice;
+
+test('get_trit', function(t) {
+  t.equal(get_trit(6, 0), 0);
+  t.equal(get_trit(6, 1), -1);
+  t.equal(get_trit(6, 2), 1);
+  t.end();
+});
+
+test('clear_trit', function(t) {
+  t.equal(n2bts(clear_trit(bts2n('1i'),0)), '10');
+  t.equal(n2bts(clear_trit(bts2n('1i'),1)),  'i');
+  t.equal(n2bts(clear_trit(bts2n('iii'),0)), 'ii0');
+  t.equal(n2bts(clear_trit(bts2n('iii'),1)), 'i0i');
+  t.equal(n2bts(clear_trit(bts2n('iii'),2)),  'ii');
+  t.end();
+});
+
+test('set_trit', function(t) {
+  t.equal(n2bts(set_trit(bts2n('1i'), 0, 1)), '11');
+  t.equal(n2bts(set_trit(bts2n('1i'), 0, 0)), '10');
+  t.equal(n2bts(set_trit(bts2n('1i'), 0,-1)), '1i');
+  t.equal(n2bts(set_trit(bts2n('1i'), 1, 0)),  'i');
+  t.equal(n2bts(set_trit(bts2n('1i'), 1,-1)), 'ii');
+  t.end();
+});
+
+test('trit_slice', function(t) {
+  t.equal(trit_slice(bts2n('111000iii'), 0, 3), bts2n('iii'));
+  t.equal(trit_slice(bts2n('111000iii'), 3, 6), bts2n('000'));
+  t.equal(trit_slice(bts2n('111000iii'), 6, 9), bts2n('111'));
+  t.end();
+});
 
